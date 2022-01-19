42c37b5560ee753774b769a146104f3fc2f6da9a
John Emilian Minj <64469866+john3milian@users.noreply.github.com>
2022-01-19 16:32:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="Flask",
+    name="dvt-py",
     install_requires=[
         "numpy >= 1.22",
         "pandas >= 1.3",
